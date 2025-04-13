e0cc43fb0287c367e3d274ed08842d55c9ea14e0
Yash Sonune <yash.sonune08@gmail.com>
2025-04-13 10:21:20 -0700

    LeetCode 128 - Find Longest Sequece Length of elements in Array

---
+++ DSA/arrays/find-longest-sequence/LongestSequence.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+bool linearSearch(int n, vector<int> &arr, int key) {
+  for (int i = 1; i < n; ++i) {
+    if (arr[i] == key) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int findLongestSequenceBF(int n, vector<int> &arr) {
+  int maxLen = 0;
+
+  for (int i = 0; i < n; i++) {
+    int x = arr[i];
+    int cnt = 1;
+
+    while (linearSearch(n, arr, x + 1)) {
+      cnt++;
+      x++;
+    }
+
+    if (cnt > maxLen) {
+      maxLen = cnt;
+    }
+  }
+
+  return maxLen;
+}
+
+int findLongestSequence_Better(int n, vector<int> &arr) {
+  // TC: O(nlongn + n)
+  // SC: O(1)
+
+  // Handling Edge Case
+  if (arr.size() == 0)
+    return 0;
+
+  if (arr.size() == 1)
+    return 1;
+
+  // performing sorting
+  sort(arr.begin(), arr.end());
+  int cnt = 1;
+  int maxLen = 1;
+
+  // looping through the whole array to find out maxLen
+  for (int i = 0; i < n - 1; ++i) {
+    if (arr[i] != arr[i + 1]) {
+      if (arr[i] + 1 == arr[i + 1]) {
+        cnt++;
+      } else {
+        cnt = 1;
+      }
+      maxLen = max(cnt, maxLen);
+    }
+  }
+
+  return maxLen;
+}
+
+int findLongestSequence_optim(int n, vector<int> &arr) {
+  // TC: O(n) or O(3n) -- O(n) for insert, O(n) for visit all element, O(n)
+  // while for visiting each seqquence once. SC: O(1)
+
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+
+  int maxLen = 1;
+  unordered_set<int> st;
+
+  // inserting all element in set
+  for (int i = 0; i < n; i++) {
+    st.insert(arr[i]);
+  }
+
+  // Searching for
+  for (int i = 0; i < n; i++) {
+    if (st.find(arr[i] - 1) == st.end()) {
+      int x = arr[i];
+      int cnt = 1;
+
+      while (st.find(x + 1) != st.end()) {
+        cnt++;
+        x = x + 1;
+      }
+
+      maxLen = max(cnt, maxLen);
+    }
+  }
+  return maxLen;
+}
+
+void printArray(int n, vector<int> &arr) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++) {
+      cin >> arr[i];
+    }
+
+    cout << "The Longest Sequence of the Array found is: "
+         /*<< findLongestSequenceBF(n, arr);*/
+         /*<< findLongestSequence_Better(n, arr);*/
+         << findLongestSequence_optim(n, arr);
+
+    cout << endl;
+  }
+
+  return 0;
+}
